223297e492ebdfa4b535dfb3b4f446019995d720
ganxiaofeng123 <your.email@example.com>
2024-03-31 12:36:51 +0800

    资源投送

--- app/src/main/java/com/example/fuyong/MainActivity.java
+++ app/src/main/java/com/example/fuyong/MainActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.refresh.GodRefreshLayout;
 
+public class MainActivity extends AppCompatActivity {
+    private GodRefreshLayout godRefreshLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        godRefreshLayout = findViewById(R.id.god_refresh);
+        godRefreshLayout.setRefreshManager();
     }
 }
